a3c5998287ea17a909d8757eaceb6cf665502746
Addison Schuhardt <addison@schuhardt.net>
2020-06-06 01:02:22 -0700

    stripping out functions I don't want and removing file name + line numbers from all levels but TRACE

--- src/log.h
+++ src/log.h
@@ -8,25 +8,22 @@
 #ifndef LOG_H
 #define LOG_H
 
-#include <stdio.h>
 #include <stdarg.h>
+#include <stdio.h>
 
 #define LOG_VERSION "0.1.0"
 
 typedef void (*log_LockFn)(void *udata, int lock);
 
-enum { LOG_TRACE, LOG_DEBUG, LOG_INFO, LOG_WARN, LOG_ERROR, LOG_FATAL };
+enum { LOG_TRACE, LOG_DEBUG, LOG_INFO, LOG_WARN, LOG_ERROR };
 
 #define log_trace(...) log_log(LOG_TRACE, __FILE__, __LINE__, __VA_ARGS__)
-#define log_debug(...) log_log(LOG_DEBUG, __FILE__, __LINE__, __VA_ARGS__)
-#define log_info(...)  log_log(LOG_INFO,  __FILE__, __LINE__, __VA_ARGS__)
-#define log_warn(...)  log_log(LOG_WARN,  __FILE__, __LINE__, __VA_ARGS__)
-#define log_error(...) log_log(LOG_ERROR, __FILE__, __LINE__, __VA_ARGS__)
-#define log_fatal(...) log_log(LOG_FATAL, __FILE__, __LINE__, __VA_ARGS__)
+#define log_debug(...) log_log(LOG_DEBUG, NULL, -1, __VA_ARGS__)
+#define log_info(...) log_log(LOG_INFO, NULL, -1, __VA_ARGS__)
+#define log_warn(...) log_log(LOG_WARN, NULL, -1, __VA_ARGS__)
+#define log_error(...) log_log(LOG_ERROR, NULL, -1, __VA_ARGS__)
 
 void log_set_udata(void *udata);
-void log_set_lock(log_LockFn fn);
-void log_set_fp(FILE *fp);
 void log_set_level(int level);
 void log_set_quiet(int enable);
 
